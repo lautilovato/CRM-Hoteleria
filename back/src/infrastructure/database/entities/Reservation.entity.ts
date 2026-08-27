631d3af1167dd923b0ecee6dcaa881fa7345f663
Lucrecia <lucrecolon1828@gmail.com>
2026-08-27 12:15:03 -0300

    feat: agrego tablas de room, room-category y reservation

---
+++ back/src/infrastructure/database/entities/Reservation.entity.ts
@@ -0,0 +1,35 @@
+import { Collection } from '@mikro-orm/core'; 
+import { Entity, PrimaryKey, Property, OneToMany, ManyToOne, Enum } from '@mikro-orm/decorators/legacy';
+import { v4 } from 'uuid';
+import { Room } from './Room.entity';
+import { CustomBaseEntity } from './CustomBase.entity';
+
+export enum ReservationStatus {
+  PENDING_PAYMENT = 'PENDING_PAYMENT',
+  CONFIRMED = 'CONFIRMED',
+  CANCELLED = 'CANCELLED',
+}
+
+@Entity({ tableName: 'reservations' })
+export class Reservation extends CustomBaseEntity {
+  @PrimaryKey({ type: 'uuid' })
+  id: string = v4();
+
+  @ManyToOne(() => Room)
+  room!: Room;
+
+  @Property({ type: 'varchar' })
+  telegramUserId!: string;
+
+  @Property({ type: 'date' })
+  checkIn!: Date;
+
+  @Property({ type: 'date' })
+  checkOut!: Date;
+
+  @Enum(() => ReservationStatus)
+  status: ReservationStatus = ReservationStatus.PENDING_PAYMENT;
+
+  @Property({ type: 'decimal', precision: 5, scale: 2 })
+  totalAmount!: number;
+}
---
+++ back/src/infrastructure/database/entities/Room.entity.ts
@@ -0,0 +1,30 @@
+import { Collection } from '@mikro-orm/core'; 
+import { Entity, PrimaryKey, Property, OneToMany, ManyToOne, Enum } from '@mikro-orm/decorators/legacy';
+import { v4 } from 'uuid';
+import { RoomCategory } from './RoomCategory.entity';
+import { CustomBaseEntity } from './CustomBase.entity';
+import { Reservation } from './Reservation.entity';
+
+export enum RoomStatus {
+  ACTIVE = 'ACTIVE',
+  MAINTENANCE = 'MAINTENANCE',
+  INACTIVE = 'INACTIVE',
+}
+
+@Entity({ tableName: 'rooms' })
+export class Room extends CustomBaseEntity {
+  @PrimaryKey({ type: 'uuid' })
+  id: string = v4();
+
+  @ManyToOne(() => RoomCategory)
+  category!: RoomCategory;
+
+  @Property({ type: 'varchar', unique: true })
+  roomNumber!: string;
+
+  @Enum(() => RoomStatus)
+  status: RoomStatus = RoomStatus.ACTIVE;
+
+  @OneToMany(() => Reservation, reservation => reservation.room)
+  reservations = new Collection<Reservation>(this);
+}
